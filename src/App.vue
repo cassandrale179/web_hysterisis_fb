1c6d9879e27998d3de0ef6932bd70ee5aa72ed24
Cassandra Le <lenguyetminh1998@gmail.com>
2019-06-09 20:31:41 -0400

    Plug back intro page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
-    <div id="appnavbar">
-      <router-link to="/hello">Introduction</router-link>
-      <router-link to="/sign-in">Experiment</router-link>
-      <router-link to="/sign-up">Data</router-link>
+
+    <!-- Navigation Bar -->
+    <div id="A">
+      <!-- <router-link class="A1" to="/hello">Introduction</router-link>
+      <router-link class="A1" to="/sign-in"> Experiment</router-link>
+      <router-link class="A1" to="/sign-up"> Data</router-link> -->
     </div>
+
+
     <router-view/>
   </div>
 </template>
@@ -38,22 +42,56 @@ export default {
     position: absolute;
     height: 100%; 
     width: 100%; 
-    background-image: url('https://free4kwallpapers.com/uploads/originals/2019/05/18/firewatch-wallpaper.jpg'); 
+    background-image: url('http://i.imgur.com/4Pp7dRg.jpg');   
     background-size: cover;  
     text-align: center; 
     color: white; 
+    font-family: 'Roboto'; 
   }
 
 
-/* Native elements styling */ 
-a{
-  color: white; 
-}
+  /* Native elements styling */ 
+  a{
+    color: white; 
+  }
 
-  #appnavbar{
+  #A{
+    padding: 0.7em; 
+    height: 3em; 
+    text-align: left; 
     font-family: 'Roboto', sans-serif; 
     color: white; 
     font-size: 20px;
   }
+
+  .A1{
+    padding-right: 0.5em; 
+  }
+
+  /* Material design button */ 
+  .btn{
+    margin-top: 20px;
+    position: relative;
+    display: inline-block;
+    box-sizing: border-box;
+    border: none;
+    border-radius: 4px;
+    padding: 0 16px;
+    min-width: 64px;
+    height: 36px;
+    text-align: center;
+    text-overflow: ellipsis;
+    text-transform: uppercase;
+    background-color: black; 
+    box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+    font-weight: 500;
+    line-height: 36px;
+    overflow: hidden;
+    outline: none;
+    cursor: pointer;
+    transition: box-shadow 0.2s;
+    color: white; 
+  }
+
   
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,48 @@
 <!--- HTML tempalte for the HelloWorld.vue  --> 
 
 <template>
-  <div class="hello">
-      <div class="container">
+  <div v-if="count > 0" class="hello">
+      <div id="container">
+
+        <div v-if= "page < 5" >
         <h3>Introduction</h3>
+        <div id="text"> In this task, press <b> t </b> if there are more triangles, press <b> c </b> if there are more circles. 
+          For <a href="" class="blue"> blue-colored</a> shape, focus on the <b> big </b> shapes. 
+          For  <a href="" class="green"> green-colored</a>  shape, focus on the <b>small </b> shapes 
+          that make up the big shapes.
+        </div>
+
+           <!-- Instructions example (4 slide in total ) -->
+           <div v-if="page <= 1" class="example">
+              <img src="../assets/d1.png" /> 
+              <u>Example 1 (blue = big):</u> There are <b> 4 triangles </b> and <b> 6 circles </b> in this picture. Press <b>c</b> 
+          </div>
+          <div v-if="page === 2" class="example">
+              <img src="../assets/d2.png" /> 
+              <u>Example 2 (blue = big):</u> There are <b> 8 triangles </b> and <b> 2 circles </b> in this picture. Press <b>t</b> 
+          </div>
+          <div v-if="page === 3" class="example">
+              <img src="../assets/d3.png" /> 
+              <u>Example 3 (green = small):</u> There are <b> 4 triangles </b> and <b> 6 circles </b> in this picture. Press <b>c</b> 
+          </div>
+          <div v-if="page === 4" class="example">
+              <img src="../assets/d4.png" /> 
+              <u>Example 4 (green = small):</u> There are <b> 8 triangles </b> and <b> 2 circles </b> in this picture. Press <b>t</b> 
+          </div> 
+
+          <button  v-on:click="page -= 1" class="btn"> << Back</button>
+          <button  v-on:click="page += 1" class="btn"> Next >> </button> </div>
+
+          <!-- ____________  BEGIN EXPERIMENT HERE  _______________ --> 
+          <div v-if= "page >= 5" >
+            <h1> Expeirment </h1>  
+            The experiment will begin. You will see <b>10 </b> slides of just blue shapes,  <b>10 </b> slieds of green shapes, 
+            then  <b>10 </b> slides of blue, green, blue, green ...etc.  <br > 
+            Click <b> Begin </b> when you are ready, or <b> Back </b>to review the examples.  <br><br>
+            <button class="btn" v-on:click="page -= 1" > << Back </button> 
+            <button class="btn" v-on:click="setCurrTime" > Begin </button> 
+            <h2> {{count}} seconds </h2>
+          </div>
         
       </div>    
   </div>
@@ -15,11 +54,24 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
+      count: 5, 
+      page : 1,  
       fireData: null, 
     }
   },
 
   methods:{
+
+    setCurrTime: function(){
+      this.intervalid1 = setInterval(function(){
+        this.$data.count -= 1; 
+        if (this.$data.count <= 0){
+          this.$emit('eventname', this.$data.count);  
+          clearInterval(this.intervalid1); 
+        } 
+      }.bind(this), 1000);
+    
+    }, 
 
     // Fetch data from Firebase 
     fetchFirebaseData(){
@@ -39,4 +91,40 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  #container{
+    padding: 2em; 
+    height: 45em;
+    background: white; 
+    border-radius: 4px; 
+    margin-left: 20em; 
+    margin-right: 20em; 
+    margin-top: 3em;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    color: #283747;
+  }
+
+  #text{
+    padding-left: 5em;
+    padding-right: 5em;
+    font-size: 1em;
+  }
+
+  .blue{
+    font-weight: bold;
+    color: #2471A3; 
+  }
+  .green{
+    font-weight: bold;
+    color: #16A085;     
+  }
+
+  u{
+    display: block;
+  }
+
+  img{
+    width: 600px; 
+  }
+
+
 </style>
